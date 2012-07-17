a0ebbb5ef1b3df2c27a2dbb6fe15fc1a3b4cabb8
Jenna <starrjenna@me.com>
2012-07-17 11:44:44 -0500

    Laying out

--- sdi-project3/starr_jenna_assignment3.js
+++ sdi-project3/starr_jenna_assignment3.js
@@ -8,3 +8,61 @@ alert("JavaScript works!");
 //global variable
 
 var motivation = 
+
+// loop x3
+
+/* for loop
+ for (i=0; i<5; i++)
+  {
+  x=x + "The number is " + i + "<br />";
+  }
+  
+  while loop
+  while (i<5)
+  {
+  x=x + "The number is " + i + "<br />";
+  i++;
+  }
+*/
+
+var student = {
+	id: "1298304",
+	name: "heidi",
+	interests: [
+	"Running",
+	"Eating Brains",
+	"Groaning"
+	],
+	age: 32,
+	sayHi: function () {
+		console.log ("Brainsssssss");
+	}
+};
+var key = "name";
+student.name = "Heidi";
+
+console.log(student[key] );
+console.log(student["sayHi"] );
+console.log(student.sayHi );
+
+student.sayHi();
+
+student.sayHi= function() {
+	console.log("Uhhhhhhh");
+};
+
+student["sayHi"]();
+
+for (var key in student) {
+	console.log("key: " + key + ", value: ", student[key]);
+};
+
+
+
+// argument x5
+
+// method x4
+
+// property x5
+
+// return x5
