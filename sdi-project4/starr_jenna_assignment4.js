c109dd7f184730657699b350da2d80cf9fa66e34
Jenna <starrjenna@me.com>
2012-07-25 05:51:09 -0500

    Phone Number
    Commented out area to explain. Unable to get console logs to portray
    message. Continuing to next problem will come back and re-check.

--- sdi-project4/starr_jenna_assignment4.js
+++ sdi-project4/starr_jenna_assignment4.js
@@ -1,16 +1,23 @@
 alert("JavaScript works!");
 
 // Does a string follow a 123-456-7890 pattern like a phone number?
+/*
+ With this code we are providing a string of numbers in a phone number format and
+ have it go through a series of steps to validate that it does or does not meet the
+ parameters of a phone number.
+*/
+numberChecking = ("8166-867-5209");
 
-var numberCheck = function(lookDigit){
-	var testNumber = lookDigit;
-	var digits = /^\(?([0-9]{3})\)?[-.]?([0-9]{3})[-.]?([0-9]{4})$;
-
-if (digits.test(testNumber)){
-	var realNumber = testNumber.replace
-	console.log("You are right, it is a real phone number!");
-	}
-	else{
-		return console.log("No, that is not a phone number.");
-	}
-};
+var numberChecking = function (lookDigits) {
+        var phoneNumber = lookDigits;
+        // using the var listing has it look through the three sections of digits to verify it is a phone number.
+        var listing = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; 
+        if (listing.test(validPhoneNumber)) { 
+            var realNumber = validPhoneNumber.replace(pattern, "($1) $2-$3");
+            
+            //console log gives a string validation if it is either correct or incorrect.
+            console.log("Valid phone number!");
+        } else {
+            return console.log("Invalid phone number!"); 
+        }
+    }
