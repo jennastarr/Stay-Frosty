a64c0dc931ed4f9d8f34cc6ace4882c8bf4b1829
Jenna <starrjenna@me.com>
2012-07-26 16:34:18 -0500

    Comments and transposing.
    Transposing my previous code into the new library.

--- sdi-project4/starr_jenna_assignment4.js
+++ sdi-project4/starr_jenna_assignment4.js
@@ -30,12 +30,84 @@ the limit of decimal places you would like shown.
 		};
 	};
 	
+// 3. Title-case a string (split into words, then uppercase the first letter of each word.)
+/*
+Providing a phrase or statement where you want just the first letter of each word to be
+uppercase is not something you have to go through and do manually. You can provide the 
+location of which letters you wold like to be made uppercase within this function.
+*/
+fixCase("zombie nation");
+function fixCase(fixThis) {
+    String.prototype.toProperCase = function () {
+
+        return this.replace(/\w\S*/g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    };
+	fixThis.toProperCase();
+    console.log(fixThis.toProperCase()); 
+};
+
+//  4. Is the string a URL? (Does it start with http: or https:?)
+/*
+We give a proper link within this code to be analyzed in the beginning. For the console
+log we concatenate to confirm or deny if it is a http or https. This is done by telling
+the code to look at the 4th location and if it sees one thing or another to log out
+said string.
+This code also does another job by identifying if it is a true URL.
+*/
+var urlString = function (urlTest, url){
+		if (location.protocol === 'http:')
+		return library.check = url.check(urlCheck)
+		return library.check = "http://www.fullsail.com"
+	};
+	
+/* 
+I re-watched videos and found another way of doing this problem but i prefer the
+information the code below provides. I did not know how to get the codes combined to
+give both things.
+*/	
+checkUrl("http://www.fullsail.com/");
+
+function checkUrl(testLink) {
+    var url = testLink
+    var link = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    var isLink = link.test(url);
+    console.log("This is a " + isLink + " URL.");
+    if (url.charAt(4) == "s") {
+        console.log("This is a https URL");
+    }
+    if (url.charAt(4) == ":") {
+        console.log("This is a http URL");
+    };
+};
+
+// 5. Does a string follow an aaa@bbb.ccc pattern like an email address?
+/*
+Given an email address, parameters are given within the code to check against the email
+address. If these are met then console log will give you a negative or affirmative response.
+*/
+
+var emailAddy = function ( emailCheck ){
+		var atCheker  = emailCheck.indexOf("@");
+		var dotCheker = emailCheck.indexOf(".");
+		if (atCheker != -1 && dotCheker != -1) {
+			return library.emailCheck = "checkyoself@prodigy.com";
+		} else {
+			return library.emailCheck = "invalid";
+		};
+	};
+	
+
+	
 	
 //Return Sector
 
 	return{
 	"decimal": decimal,
 	"phoneNum": phoneNum,
+	"urlString": urlString,
+	"emailAddy": emailAddy,
 	};
 };
 	
@@ -43,11 +115,16 @@ the limit of decimal places you would like shown.
 // Library resource. Calling point for the code.	
 var library = libRary();
 
-// Problem 1
 library.decimal(723.1980);
 
-// Problem 2
 library.phoneNum(true);
+
+library.urlString(true);
+
+library.emailAddy("checkyoself@prodigy.com");
+
+
+
 
 console.log(library);
 
