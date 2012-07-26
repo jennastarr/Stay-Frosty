3a3d8af96c76338785141f07d1870473738945cf
Jenna <starrjenna@me.com>
2012-07-26 16:01:45 -0500

    Additions
    Adding a library and return section instead of having things within the
    function.

--- sdi-project4/starr_jenna_assignment4.js
+++ sdi-project4/starr_jenna_assignment4.js
@@ -1,6 +1,22 @@
-alert("Project 4 the ReMix!);
+alert("Project 4 the ReMix!");
 
-// 1. Does a string follow a 123-456-7890 pattern like a phone number?
+
+// 2. Format a number to use a specific number of decimal places, as for money: 2.1 ➜ 2.10
+/*
+A code for displaying limited decimal points. This code acts as a boundary set when displaying
+number with numerous decimal points. You are able to simply adjust the "toFixed" are to set
+the limit of decimal places you would like shown.
+*/
+
+	var libRary = function(){
+	var decimal = function ( deciNumber ){
+		var deciNum = 723.1980;
+		return library.deciNumber = deciNum.toFixed(2)
+	};
+
+
+
+// 2. Does a string follow a 123-456-7890 pattern like a phone number?
 /*
  With this code we are providing a string of numbers in a phone number format and
  have it go through a series of steps to validate that it does or does not meet the
@@ -11,5 +27,22 @@ alert("Project 4 the ReMix!);
 		return library.number = 816-867-5309;
 		} else {
 		return library.number = false;
-		}
+		};
 	};
+	
+	
+//Return Sector
+
+	return{
+	"decimal": decimal,
+	"phoneNum": phoneNum,
+	
+	
+// Library resource. Calling point for the code.	
+var library = libRary();
+
+// Problem 1
+library.decimal(723.1980);
+
+// Problem 2
+library.phoneNum(true);
