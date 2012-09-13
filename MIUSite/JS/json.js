89b325b27b77ba1c3f6c1507c57de43b694472c3
Jenna <starrjenna@me.com>
2012-09-13 16:24:39 -0700

    Functional
    local storage issue fixed. Dummy data populating.

--- MIUSite/JS/json.js
+++ MIUSite/JS/json.js
@@ -1,149 +1,304 @@
-//Jenna Starr
-//MiU
-//Web App
-//1208
+
 
 
 var json = {
+	"archive1": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Shotgun"],
+		"found": ["Found: ", "02-02-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "3"], 
+		"notes": ["Notes: ", "Yes"]
 
-	"choice1" : {
-					"choicetype"			:["Choice Type:", "Food"],
-					"undead"				:["Undead:","RUNNNNNN!!!!!"],
-								
-					},
-					
-	"choice2" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","No Worries."],
-								
-					},
-					
-	"choice3" : {
-					"choicetype"			:["Choice Type:", "Weapon"],
-					"undead"				:["Undead:","AHHHHHHH!!!!"],
-								
-					},
-						},
-					
-	"choice4" : {
-					"choicetype"			:["Choice Type:", "Food"],
-					"undead"				:["Undead:","I think we are going to need a bigger bat!"],
-								
-					},
-						},
-					
-	"choice5" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","Yes"],
-								
-					},
-						},
-					
-	"choice6" : {
-					"choicetype"			:["Choice Type:", "Weapon"],
-					"undead"				:["Undead:","No"],
-								
-					},
-						},
-					
-	"choice7" : {
-					"choicetype"			:["Choice Type:", "Weapon"],
-					"undead"				:["Undead:","Why is that guy wearing a hockey mask?"],
-								
-					},
-						},
-					
-	"choice8" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","Wait i hear music, don't go in there?"],
-								
-					},
-						},
-					
-	"choice9" : {
-					"choicetype"			:["Choice Type:", "Food"],
-					"undead"				:["Undead:","AHHHHHHH!!!!"],
-								
-					},
-						},
-					
-	"choice10" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","RUNNNNNN!!!!!"],
-								
-					},
-						},
-					
-	"choice11" : {
-					"choicetype"			:["Choice Type:", "Weapon"],
-					"undead"				:["Undead:","Wait i hear music, don't go in there?"],
-								
-					},
-						},
-					
-	"choice12" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","AHHHHHHH!!!!"],
-								
-					},
-						},
-					
-	"choice13" : {
-					"choicetype"			:["Choice Type:", "Food"],
-					"undead"				:["Undead:","No Worries."],
-								
-					},
-						},
-					
-	"choice14" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","No"],
-								
-					},
-						},
-					
-	"choice15" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","No Worries."],
-								
-					},
-						},
-					
-	"choice16" : {
-					"choicetype"			:["Choice Type:", "Weapon"],
-					"undead"				:["Undead:","I think we are going to need a bigger bat!"],
-								
-					},
-						},
-					
-	"choice17" : {
-					"choicetype"			:["Choice Type:", "Food"],
-					"undead"				:["Undead:","AHHHHHHH!!!!"],
-								
-					},
-						},
-					
-	"choice18" : {
-					"choicetype"			:["Choice Type:", "Shelter"],
-					"undead"				:["Undead:","Wait i hear music, don't go in there?"],
-								
-					},
-						},
-					
-	"choice19" : {
-					"choicetype"			:["Choice Type:", "Weapon"],
-					"undead"				:["Undead:","AHHHHHHH!!!!"],
-								
-					},
-						},
-					
-	"choice20" : {
-					"choicetype"			:["Choice Type:", "Food"],
-					"undead"				:["Undead:","Why is that guy wearing a hockey mask?"],
-								
-					}				
-						
-		}
+	},
+	"archive2": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Shack"],
+		"found": ["Found: ", "01-13-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "It is what it is."]
+
+	},
+	"archive3": {
+		"groups": ["Category: ", "Food"],
+		"atitle": ["Item: ", "Chicken"],
+		"found": ["Found: ", "02-18-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "1 stars"],
+		"slider": ["Slider: ", "2"], 
+		"notes": ["Notes: ", "Are you sure this is chicken?"]
+
+	}, 
+	"archive4": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Shovel"],
+		"found": ["Found: ", "02-08-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made", "Yes"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "3"], 
+		"notes": ["Notes: ", "Yea but it has duel purposes."]
+
+	},
+	"archive5": {
+		"groups": ["Category: ", "Food"],
+		"atitle": ["Item: ", "Dog Food"],
+		"found": ["Found: ", "01-15-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "No"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "Again?!"]
+
+	},
+	"archive6": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Tarp"],
+		"found": ["Found: ", "02-20-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "10"], 
+		"notes": ["Notes: ", "There are holes in this!"]
+
+	},
+	"archive7": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Machete"],
+		"found": ["Found: ", "02-15-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "3"], 
+		"notes": ["Notes: ", "Why is there a guy in a hockey mask?"]
+
+	},
+	"archive8": {
+		"groups": ["Category: ", "Food"],
+		"atitle": ["Item: ", "Taco Bell"],
+		"found": ["Found: ", "01-05-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "Yes"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "Dog food again?!"]
+
+	},
+	"archive9": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Cabin"],
+		"found": ["Found: ", "02-19-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "10"], 
+		"notes": ["Notes: ", "This Crystal Lake place is nice."]
+
+	},
+	"archive10": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Bat"],
+		"found": ["Found: ", "02-11-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "Yes"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "5"], 
+		"notes": ["Notes: ", "Swing for the field!"]
+
+	},
+	"archive11": {
+		"groups": ["Category: ", "Food"],
+		"atitle": ["Item: ", "Spam"],
+		"found": ["Found: ", "01-16-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "Yes"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "No"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "I don't feel so good..."]
+
+	},
+	"archive12": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Butter Knife"],
+		"found": ["Found: ", "02-18-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "10"], 
+		"notes": ["Notes: ", "What am I supposed to do with this?"]
+
+	},
+	"archive13": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Basement"],
+		"found": ["Found: ", "02-28-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "3"], 
+		"notes": ["Notes: ", "Wait i hear creepy music...don't go in there."]
+
+	},
+	"archive14": {
+		"groups": ["Category: ", "Food"],
+		"atitle": ["Item: ", "Chips"],
+		"found": ["Found: ", "01-20-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "No"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "What's with the chalk drawings all over."]
+
+	},
+	"archive15": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Shopping Cart"],
+		"found": ["Found: ", "02-21-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "10"], 
+		"notes": ["Notes: ", "So uncomfortable."]
+
+	},
+	"archive16": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Chainsaw"],
+		"found": ["Found: ", "02-02-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "Yes"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "3"], 
+		"notes": ["Notes: ", "Should I be wearing protective gear?"]
+
+	},
+	"archive17": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Cave"],
+		"found": ["Found: ", "01-02-2012"],
+		"checkbox1": ["Taken: ", "No"],
+		"checkbox2": ["Made: ", "Yes"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "Night light anyone?"]
+
+	},
+	"archive18": {
+		"groups": ["Category: ", "Food"],
+		"atitle": ["Item: ", "Liver"],
+		"found": ["Found: ", "02-20-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "10"], 
+		"notes": ["Notes: ", "Animal or human, we aren't sure."]
+
+	},
+	"archive19": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item:", "Trailer"],
+		"found": ["Found: ", "02-22-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "1 stars"],
+		"slider": ["Slider: ", "1"], 
+		"notes": ["Notes: ", "Great zombiefied and white trash."]
+
+	},
+	"archive20": {
+		"groups": ["Category: ", "Weapon"],
+		"atitle": ["Item: ", "Duct Tape"],
+		"found": ["Found: ", "01-16-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Helpful"],
+		"used": ["First time used: ", "No"],
+		"again": ["Use again: ", "Yes"],
+		"ratestars": ["Rate stars: ", "3 stars"],
+		"slider": ["Slider: ", "8"], 
+		"notes": ["Notes: ", "It was good enough for NASA."]
+
+	},
+	"archive21": {
+		"groups": ["Category: ", "Shelter"],
+		"atitle": ["Item: ", "Barn"],
+		"found": ["Found: ", "02-17-2012"],
+		"checkbox1": ["Taken: ", "Yes"],
+		"checkbox2": ["Made: ", "No"],
+		"acquired": ["Item was? ", "Pointless"],
+		"used": ["First time used: ", "Yes"],
+		"again": ["Use again: ", "No"],
+		"ratestars": ["Rate stars: ", "2 stars"],
+		"slider": ["Slider: ", "3"], 
+		"notes": ["Notes: ", "RUNNNN!!!!!!"]
+
+	} 
+
 
 
 } 
+
+
